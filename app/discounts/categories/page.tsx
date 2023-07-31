b851c30a5424c193a3055a6ef1f5052d312948f8
tarifalhasan <tarialhasan999@gmail.com>
2023-07-31 13:44:37 +0600

    cashback descontos funcionarios saudetelemedicina done

--- app/discounts/categories/page.tsx
+++ app/discounts/categories/page.tsx
@@ -4,11 +4,7 @@ import { DescontosSub } from "@/constant/data";
 import AddCategories from "./add-category";
 import ListCategories from "./list-category";
 
-interface Props {
-  className?: string;
-}
-
-const Categorys: React.FC<Props> = ({ className }) => {
+const Categorys = () => {
   return (
     <div className=" space-y-5">
       <MainNavbar title="Descontos" />
@@ -17,7 +13,7 @@ const Categorys: React.FC<Props> = ({ className }) => {
         name="Categorias"
         buttonsData={DescontosSub}
       />
-      <div className={`${className} lg:grid-cols-2 grid gap-6`}>
+      <div className={`lg:grid-cols-2 grid gap-6`}>
         <div className=" bg-white rounded-14 shadow-5xl p-5">
           <h2 className="text-lg py-5 lg:text-xl font-semibold">
             Clique para adicionar uma categoria
