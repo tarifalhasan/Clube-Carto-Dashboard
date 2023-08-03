5d5eaa281dd672ad23209dbf80febc33920eeff8
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-08-03 21:49:17 +0600

    working on notification UI

--- app/notifications/email/page.tsx
+++ app/notifications/email/page.tsx
@@ -10,7 +10,7 @@ const btnState = [
   {
     id: 1,
     name: "Email",
-    active: false,
+    active: true,
   },
   {
     id: 2,
@@ -42,15 +42,15 @@ const toolbarOptions = [
 const editorContainerStyle = {
   height: "320px", // Set the desired height here (e.g., 500px).
   // You can also use other CSS properties like 'minHeight', 'maxHeight', etc.
-  // If you want the height to adjust dynamically based on content, set it to 'auto'.
-  // height: "auto",
 };
 
 function Email() {
+  const [active, setActive] = useState(1);
   const [btnGrp, setBtnGrp] = useState(btnState);
   const [value, setValue] = useState("");
 
   const toggle = (id: number) => {
+    setActive(id);
     const newBtnGrp = btnGrp.map((btn) => {
       if (btn.id === id) {
         return { ...btn, active: !btn.active };
@@ -80,27 +80,38 @@ function Email() {
         Crie uma notificação de email personalizada para o seu público
       </h2>
       <div className="py-8 flex gap-5">
-        <div className="flex flex-col gap-3 border border-gray-200 p-3 rounded-md">
-          {displayBtns}
+        <div>
+          <div className="flex flex-col gap-3 border border-gray-200 p-3 rounded-md">
+            {displayBtns}
+          </div>
         </div>
         {/* rich text editor  */}
-        <div className="w-full min-h-[420px] border rounded-md p-3">
-          <div className="mb-1 flex gap-4 font-semibold">
-            <p className="cursor-pointer py-1 px-2 border border-gray-300 rounded-lg hover:bg-blue-100 select-none">
-              Design
-            </p>
-            <p className="cursor-pointer py-1 px-2 border border-gray-300 rounded-lg hover:bg-blue-100 select-none">
-              Enviar
-            </p>
+        {active === 1 ? (
+          <div className="w-full h-[430px] border rounded-md p-3">
+            <div className="mb-2 flex gap-4 font-semibold">
+              <p className="cursor-pointer py-1 px-2 border border-gray-300 rounded-lg hover:bg-blue-100 select-none">
+                Design
+              </p>
+              <p className="cursor-pointer py-1 px-2 border border-gray-300 rounded-lg hover:bg-blue-100 select-none">
+                Enviar
+              </p>
+            </div>
+
+            <ReactQuill
+              theme="snow"
+              value={value}
+              onChange={setValue}
+              modules={{ toolbar: toolbarOptions }}
+              style={editorContainerStyle}
+            />
           </div>
-          <ReactQuill
-            theme="snow"
-            value={value}
-            onChange={setValue}
-            modules={{ toolbar: toolbarOptions }}
-            style={editorContainerStyle}
-          />
-        </div>
+        ) : active === 2 ? (
+          <div className="w-full h-[430px] border rounded-md p-3">Push</div>
+        ) : active === 3 ? (
+          <div className="w-full h-[430px] border rounded-md p-3">SMS</div>
+        ) : (
+          <div className="w-full h-[430px] border rounded-md p-3">Chat</div>
+        )}
       </div>
     </div>
   );
