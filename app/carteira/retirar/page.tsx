b6b6b268bc4e4762ae747d3a14a5e404fca73af5
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-08-07 20:16:15 +0600

    working on retiar UI

--- app/carteira/retirar/page.tsx
+++ app/carteira/retirar/page.tsx
@@ -1,11 +1,23 @@
 import SubHeader from "@/components/sub-header";
 import { carteira_routes } from "@/constant/carteira";
+import pig from "@/assets/images/pig.svg";
 import React from "react";
+import Image from "next/image";
 
 function Retirar() {
   return (
     <div>
       <SubHeader href="/retirar" name="Retirar" buttonsData={carteira_routes} />
+
+      <div className="my-8 flex justify-between items-center">
+        <h3 className="font-bold text-lg">Realize uma retirada</h3>
+        <p className="flex items-center py-1 px-3 bg-[#353A40] rounded-lg hover:bg-black cursor-pointer select-none active:bg-gray-600">
+          <Image src={pig} alt="pig" width={30} height={30} />
+          <span className="ml-1 text-[#74EEAE]">Solicitar retirada</span>
+        </p>
+      </div>
+
+      <div className="border-2 border-gray-200 rounded-md p-5">hi</div>
     </div>
   );
 }
